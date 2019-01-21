610fe114b0e56211e1e54c05661b5d3ca5885189
bhavan12 <46780437+bhavan12@users.noreply.github.com>
2019-01-21 17:57:10 +0530

    Update mydeploy.py

--- mydeploy.py
+++ mydeploy.py
@@ -1,6 +1,7 @@
 from flask import Flask
+import psycopg2
 app = Flask (__name__)
-
+con = psycopg2.connect("dbname=dfhaphi9vtlee6 user=qcmezkrwpidutn password=3fd32869bc0d2fb997e2c47dfe339f552267e7c40bc00949638ae3b43947a64d host=ec2-107-20-183-142.compute-1.amazonaws.com")
 @app.route('/')
 def index():
   return 'hello, world'
