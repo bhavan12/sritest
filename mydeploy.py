bd1bb82bdf27a05cfaebf4518511c1d5ecd93afa
bhavan12 <46780437+bhavan12@users.noreply.github.com>
2019-01-22 10:10:15 +0530

    Update mydeploy.py

--- mydeploy.py
+++ mydeploy.py
@@ -1,21 +1,8 @@
 from io import BytesIO
-#from flask import Flask, render_template, send_file, make_response
-#from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-#from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-#import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
 import psycopg2
-#import xgboost as xgb
-#from flask import Flask, render_template,request
-#from flask_bootstrap import Bootstrap
-#from sklearn.ensemble import AdaBoostRegressor
-#from sklearn.metrics import mean_squared_error
-#import pygal
-#plt.style.use('ggplot')
 app = Flask(__name__)
-#Bootstrap(app)
 con = psycopg2.connect("dbname=dfhaphi9vtlee6 user=qcmezkrwpidutn password=3fd32869bc0d2fb997e2c47dfe339f552267e7c40bc00949638ae3b43947a64d host=ec2-107-20-183-142.compute-1.amazonaws.com")
 @app.route('/')
 def index():
