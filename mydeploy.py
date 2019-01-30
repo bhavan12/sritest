2463073fa78e201363081c8c07f6ba1920938692
bhavan12 <46780437+bhavan12@users.noreply.github.com>
2019-01-30 09:58:01 +0530

    Update mydeploy.py

--- mydeploy.py
+++ mydeploy.py
@@ -13,39 +13,36 @@ from sklearn.metrics import mean_squared_error
 con = psycopg2.connect("dbname=dfhaphi9vtlee6 user=qcmezkrwpidutn password=3fd32869bc0d2fb997e2c47dfe339f552267e7c40bc00949638ae3b43947a64d host=ec2-107-20-183-142.compute-1.amazonaws.com")
 @app.route('/app1',methods=['GET', 'POST'])
 def index():
-  try:
-    if request.method == 'POST':
-      '''userDetails = request.form
-      num1= userDetails['num1']
-      num2 = userDetails['num2']
-      num3 = userDetails['num3']
-      num1 = int(num1)
-      num2 = int(num2)
-        num3 = int(num3)'''
-      num1 = request.args.get('lid')
-      num2 = request.args.get('mid')
-      num3 = request.args.get('nid')
-      sql = """select * from qms.aud2"""
-      df = pd.io.sql.read_sql(sql, con)
-      DepartmentID = df['deptid']
-      AuditorID = df['auditorid']
-      SiteID = df['siteid']
-      cper = df['cper']
-      X = np.array([AuditorID, SiteID, DepartmentID]).T
-      y = np.array(cper)
-      from sklearn.model_selection import train_test_split
-      X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.0)
-      abc = AdaBoostRegressor(n_estimators=50,
-                              learning_rate=1)
-      model = abc.fit(X_train, y_train)
-      z = np.array([num1,num2,num3]).reshape(1, -1)
-      # Predict the response for test dataset
-      y_pred = model.predict(z)
-      y_pred=int(y_pred)
-      pythonDictionary = {'a':num1, 'b':num2, 'c':num3,'e':y_pred}
-      return  json.dumps(pythonDictionary)
- except requests.exceptions.RequestException as e:
-    print(e)
+  if request.method == 'POST':
+    '''userDetails = request.form
+    num1= userDetails['num1']
+    num2 = userDetails['num2']
+    num3 = userDetails['num3']
+    num1 = int(num1)
+    num2 = int(num2)
+      num3 = int(num3)'''
+    num1 = request.args.get('lid')
+    num2 = request.args.get('mid')
+    num3 = request.args.get('nid')
+    sql = """select * from qms.aud2"""
+    df = pd.io.sql.read_sql(sql, con)
+    DepartmentID = df['deptid']
+    AuditorID = df['auditorid']
+    SiteID = df['siteid']
+    cper = df['cper']
+    X = np.array([AuditorID, SiteID, DepartmentID]).T
+    y = np.array(cper)
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.0)
+    abc = AdaBoostRegressor(n_estimators=50,
+                            learning_rate=1)
+    model = abc.fit(X_train, y_train)
+    z = np.array([num1,num2,num3]).reshape(1, -1)
+    # Predict the response for test dataset
+    y_pred = model.predict(z)
+    y_pred=int(y_pred)
+    pythonDictionary = {'a':num1, 'b':num2, 'c':num3,'e':y_pred}
+    return  json.dumps(pythonDictionary)
    # return render_template('index.html',a=num1,b=num2,c=num3,e=y_pred)
   '''else: 
     sql = """select * from qms.aud2""" 
